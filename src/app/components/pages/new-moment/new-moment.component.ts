1e92966767132dc89e5a57e64d41068f2b7da9f8
MarceloHcb <marcelohcb@msn.com>
2023-07-14 01:34:25 -0300

    pagina about

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Moment } from 'src/app/Moment';
+import { MessagesService } from 'src/app/services/messages.service';
 import { MomentService } from 'src/app/services/moment.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -9,7 +11,10 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent {
   btnText = 'Compartilhar!'
 
-  constructor(private momentService:MomentService){}
+  constructor(private momentService:MomentService,
+  private messageService:MessagesService,
+  private router:Router
+  ){}
   async createHandler(moment:Moment){
     const formData = new FormData();
     formData.append('title', moment.title);
@@ -18,6 +23,8 @@ export class NewMomentComponent {
       formData.append('image', moment.image);
     }
     await this.momentService.createMoment(formData).subscribe()
-    
+
+    this.messageService.add('Momento adicionado com sucesso!!')
+    this.router.navigate(['/'])
   }
 }
